663ab7674074c8ba2f8aa8c0ac82e3cd8e4d906c
Davidson Oliveira <74470344+dev-davidson@users.noreply.github.com>
2023-11-20 21:25:00 -0300

    Add files via upload

---
+++ BemVindoApp.py
@@ -0,0 +1,20 @@
+# 1 - Crie um exemplo de cada uma das principais tipagens de variáveis.
+
+# Tipo inteiro
+idade = 41
+
+# Tipo ponto flutuante
+altura = 1.62
+
+# Tipo string
+nome = "Davidson"
+
+# Tipo booleano
+tem_licenca = True
+
+# 2 - Crie uma solicitação de dados ao usuário.
+nome_usuario = input("Digite seu nome: ")
+
+# 3 - Imprima a solicitação anterior na tela com uma mensagem personalizada.
+print("Bem-vindo, " + nome_usuario + "!")
+
